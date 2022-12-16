b28eaaf28bf2554b002f2a38cd7ec0970d1e81b9
ken8237 <37855825+ken8237@users.noreply.github.com>
2022-12-16 11:21:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function handleEvent(event) {
   }
 
   const completion = await openai.createCompletion({
-    model: "text-embedding-ada-002",
+    model: "text-davinci-003",
     prompt: event.message.text ,
     max_tokens: 1000,
   });
