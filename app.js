d2663d43c45eb1f806181170e8526bf9aaf47084
ken8237 <37855825+ken8237@users.noreply.github.com>
2023-02-08 18:20:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function handleEvent(event) {
   }
 
   const completion = await openai.createCompletion({
-    model: "text-chat-davinci-002-20221122",
+    model: "text-davinci-003",
     prompt: event.message.text ,
     max_tokens: 1000,
   });
